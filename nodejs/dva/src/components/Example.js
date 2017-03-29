7921dc60ab0df362fb5b5728c80143af0743f9fa
hzh <hzhfire@qq.com>
2017-03-29 17:22:09 +0800

    add video.js

--- nodejs/dva/src/components/Example.js
+++ nodejs/dva/src/components/Example.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+// import video from 'video.js'
+
+
 const Example = () => {
+  // var player = videojs('my-player');
+
   return (
     <div>
       Example Page
+      
     </div>
   );
 };
