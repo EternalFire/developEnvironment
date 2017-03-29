f733303e72efaab06c81a83f9efecc1a20bf5ae6
hzh <hzhfire@qq.com>
2017-03-29 18:09:44 +0800

    update test component

--- nodejs/dva/src/components/Example.js
+++ nodejs/dva/src/components/Example.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
-// import video from 'video.js'
-
+import 'video.js'
+// import 'video-js.min.css'
+import 'video.js/dist/video-js.min.css'
 
 const Example = () => {
-  // var player = videojs('my-player');
+  var options = {
+
+  }
+
+  // var player = videojs('my-player', options, function onPlayerReady() {
+  //   videojs.log('Your player is ready!');
+   
+  //   // // In this context, `this` is the player that was created by Video.js. 
+  //   // this.play();
+   
+  //   // // How about an event listener? 
+  //   // this.on('ended', function() {
+  //   //   videojs.log('Awww...over so soon?!');
+  //   // });
+  // });
+
+  // console.log(player)
 
   return (
     <div>
       Example Page
-      
+      <video id="my-player" 
+        className="video-js"
+        controls 
+        data-setup='{}'
+      >
+        <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4"></source>
+      </video>
     </div>
   );
 };
